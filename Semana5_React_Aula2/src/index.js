dfe789c69524be97f9911c95f8a332bfc9a60d5e
monikinhanx <craveiromonica@gmail.com>
2018-10-23 22:29:31 -0300

    iniciando projeto com npm

---
+++ Semana5_React_Aula2/src/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// const linkCriarUmaConta = React.createElement('a',{
+//     className: 'link',
+//     href: '/conta',
+//     children: 'Criar uma conta'
+// });
+
+// const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
+
+function Link(props){
+    return (
+        <a className="link" href={props.url}>{props.children}</a>
+    )
+}
+
+function Botao(props){
+    let classes = 'botao'
+
+    if (props.desabilitado){
+        classes += " botao--desabilitado"
+    }
+
+    return (
+        <input type="button" className={classes} value={props.children} />
+    )
+}
+
+const pagLogin = (
+    <main className="pagina-login">
+        <h1>Conta</h1>
+        <p>Entre com seu email e senha.</p>
+
+        <Botao>Enviar</Botao>
+        <Botao desabilitado>Enviar</Botao>
+        <Link url="/conta">Criar uma conta</Link>
+        <Link url="/login">Fazer login</Link>
+        <Link url="/xuxu">xuxu</Link>
+    </main>
+)
+
+// const props = {
+//     url: '/conta',
+//     children: 'Criar uma conta'
+// }
+
+// const linkFazerLogin = React.createElement('a',{
+//     className: 'link',
+//     href: '/login',
+//     children: 'Fazer login'
+// });
+
+// const linkFazerLogin = <a class="link" href="/login">Fazer login</a>
+
+// const formLogin = React.createElement('form',{
+//     children:[linkCriarUmaConta, linkFazerLogin]
+// });
+
+// const formLogin = (
+//     <form>
+//         {[linkCriarUmaConta,linkFazerLogin]}
+//     </form>
+// )
+
+const divProjeto = document.getElementById('projeto');
+ReactDOM.render(pagLogin, divProjeto);
