bafe9c4126a4c130dce1f66c1a6e54157364e007
monikinhanx <craveiromonica@gmail.com>
2018-10-25 21:53:42 -0300

    iniciando classe no componente campo

--- Semana5_React_Aula2/src/componentes/Campo/Campo.js
+++ Semana5_React_Aula2/src/componentes/Campo/Campo.js
@@ -5,10 +5,25 @@ import './Campo.css'
 <input type="text" className="caixa-texto" name="email" id="email">
 */
 
-function Campo(props){
-    return(
-        <input type={props.type} className="caixa-texto" name={props.name} id={props.id} placeholder={props.placeholder} />
-    )
+// function Campo(props){
+//     return(
+//         <input type={props.type} className="caixa-texto" name={props.name} id={props.id} placeholder={props.placeholder} />
+//     )
+// }
+
+class Campo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            erro: ''
+        }
+    }
+
+    render() {
+        return(
+            <input type={this.props.type} className="caixa-texto" name={this.props.name} id={this.props.id} placeholder={this.props.placeholder} />
+        )
+    }
 }
 
 export default Campo
