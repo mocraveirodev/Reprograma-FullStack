f38dfa1494720cdf3923023d088579092f41510b
monikinhanx <craveiromonica@gmail.com>
2018-11-05 22:23:20 -0200

    adicionando redux no App

--- Semana5_React_Aula2/src/index.js
+++ Semana5_React_Aula2/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { withRouter, BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { Provider, connect } from 'react-redux'
+import store from './redux/store'
 import Navbar from './componentes/Navbar/Navbar';
 import Home from './paginas/Home/Home';
 import Login from './paginas/Login/Login';
@@ -10,45 +12,88 @@ import Contato from './paginas/Contato/Contato';
 import NaoEncontrada from './paginas/NaoEncontrada/NaoEncontrada';
 import './index.css'
 
-let usuario = JSON.parse(localStorage.getItem('usuario'))
+// let usuario = JSON.parse(localStorage.getItem('usuario'))
 
-function logaUsuario(dados) {
-    const json = JSON.stringify(dados)
-    localStorage.setItem('usuario', json)
-    usuario = dados
-    console.log('usuario', usuario)
-}
+// function logaUsuario(dados) {
+//     const json = JSON.stringify(dados)
+//     localStorage.setItem('usuario', json)
+//     usuario = dados
+//     console.log('usuario', usuario)
+// }
 
-function deslogaUsuario(){
-    localStorage.removeItem('usuario')
-    usuario = null
-}
+// function deslogaUsuario() {
+//     localStorage.removeItem('usuario')
+//     usuario = null
+// }
 
-function App(){
+function App(props) {
+    const usuario = props.usuario
+    const logaUsuario = props.logaUsuario
+    const deslogaUsuario = props.deslogaUsuario
+
     return (
         <div className="app">
             <Navbar usuario={usuario} deslogaUsuario={deslogaUsuario} />
             <Switch>
                 <Route path='/' exact render={() => {
                     return usuario ? <Home /> : <Redirect to="/login" />
-                }}/>
+                }} />
                 <Route path='/login' render={(props) => {
                     return <Login historico={props.history} onEnviar={logaUsuario} />
-                }}/>
-                <Route path='/conta' component={Conta}/>
-                <Route path='/quemsomos' component={QuemSomos}/>
-                <Route path='/contato' component={Contato}/>
-                <Route component={NaoEncontrada}/>
+                }} />
+                <Route path='/conta' component={Conta} />
+                <Route path='/quemsomos' component={QuemSomos} />
+                <Route path='/contato' component={Contato} />
+                <Route component={NaoEncontrada} />
             </Switch>
         </div>
     )
 }
 
+function passaDadosDoEstadoParaMeuComponente(state) {
+    const props = {
+        usuario: state.usuario
+    }
+
+    return props
+}
+
+function PassaFuncoesQueDisparamAcoesViaProps(dispatch) {
+    const props = {
+        logaUsuario: (dados) => {
+            const acao = {
+                type: 'LOGA_USUARIO',
+                dados: dados
+            }
+            dispatch(acao)
+        },
+        deslogaUsuario: () => {
+            const acao = {
+                type: 'DESLOGA_USUARIO'
+            }
+            dispatch(acao)
+        }
+    }
+
+    return props
+}
+
+const conectaNaStore = connect(
+    passaDadosDoEstadoParaMeuComponente,
+    PassaFuncoesQueDisparamAcoesViaProps
+)
+
+const AppConectada = withRouter( conectaNaStore(App) )
+
 // const divProjeto = document.getElementById('projeto');
 ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter>, document.getElementById('projeto'));
+    <Provider store={store}>
+        <BrowserRouter>
+            <AppConectada />
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById('projeto')
+);
 
 // const linkCriarUmaConta = React.createElement('a',{
 //     className: 'link',
